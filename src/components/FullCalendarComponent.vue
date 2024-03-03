651281caf1838dc950f719cccb2c83cf6be1eb60
jame piyawat <piyawat5930302600@gmail.com>
2024-03-03 22:18:48 +0700

    fullcalendar additional

--- src/components/FullCalendarComponent.vue
+++ src/components/FullCalendarComponent.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="full-calendar">
+    <!-- <button @click="toggleWeekends">weekends</button> -->
+    <FullCalendar ref="fullCalendar" :options="calendarOptions">
+      <template v-slot:eventContent="arg">
+        <div style="overflow: hidden; font-size: 10px">
+          <b>{{ arg.event?.title }}</b>
+          <div>{{ arg.event?.start?.toLocaleString() }}</div>
+          <div>{{ arg.event?.end?.toLocaleString() }}</div>
+        </div>
+      </template>
+    </FullCalendar>
     <v-form class="form" v-model="form" @submit.prevent="handleSubmit">
       <v-col cols="6" style="margin: auto">
         <v-text-field
@@ -27,16 +37,6 @@
         <v-btn :disabled="!form" :loading="loading" type="submit">SENT</v-btn>
       </div>
     </v-form>
-    <button @click="toggleWeekends">weekends</button>
-    <FullCalendar ref="fullCalendar" :options="calendarOptions">
-      <template v-slot:eventContent="arg">
-        <div style="overflow: hidden; font-size: 10px">
-          <b>{{ arg.event?.title }}</b>
-          <div>{{ arg.event?.start?.toLocaleString() }}</div>
-          <div>{{ arg.event?.end?.toLocaleString() }}</div>
-        </div>
-      </template>
-    </FullCalendar>
   </div>
 </template>
 
@@ -44,7 +44,13 @@
 import FullCalendar from "@fullcalendar/vue3";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
-import { CalendarOptions, DateSpanApi, EventInput } from "@fullcalendar/core";
+import {
+  CalendarOptions,
+  DateSelectArg,
+  DateSpanApi,
+  DateUnselectArg,
+  EventInput,
+} from "@fullcalendar/core";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import { ref, Ref } from "vue";
 import { reactive } from "vue";
@@ -55,16 +61,17 @@ export default {
     FullCalendar,
   },
   setup() {
+    const getSelectValue: Ref<Partial<DateSpanApi>> = ref({});
     const calendarOptions: Ref<CalendarOptions> = ref({
       plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
       initialView: "timeGridWeek",
       // timeZone: "LOCALE",
-      views: {
-        timeGridFourDay: {
-          type: "timeGrid",
-          duration: { day: 4 },
-        },
-      },
+      // views: {
+      //   timeGridWeek: {
+      //     type: "timeGridWeek",
+      //     duration: { day: 3 },
+      //   },
+      // },
       slotDuration: "00:30",
       slotMinTime: "8:00",
       slotMaxTime: "20:00",
@@ -89,42 +96,121 @@ export default {
       },
 
       selectAllow: (selectInfo: DateSpanApi) => {
-        console.log(selectInfo.start);
+        getSelectValue.value = {
+          start: selectInfo.start,
+          end: selectInfo.end,
+        };
 
-        return false;
+        if (selectInfo.start < new Date()) {
+          return false;
+        }
+        // console.log(
+        //   getSelectValue.value.start?.toLocaleString() +
+        //     "-" +
+        //     getSelectValue.value?.end?.toLocaleString()
+        // );
+
+        return true;
       },
+      select: (selectionInfo: DateSelectArg) => {
+        // calendarOptions.value = {
+        //   ...calendarOptions.value,
+        //   events: [
+        //     ...(calendarOptions.value?.events as EventInput[]),
+        //     {
+        //       start: selectionInfo.start?.toISOString(),
+        //       end: selectionInfo?.end?.toISOString(),
+        //       backgroundColor: "#CE1212",
+        //       borderColor: "#CE1212",
+        //     },
+        //   ],
+        // };
+        // console.log(calendarOptions.value);
+        // const test = prompt("title");
+        // if (test) {
+        //   alert("post api!!!");
+        // } else {
+        //   alert("get api");
+        // }
+        console.log(selectionInfo);
+      },
+      unselect: (jsEvent?: DateUnselectArg) => {
+        // doesn’t happen when unselectAuto is false
+        console.log(`${jsEvent} `);
+      },
+      nowIndicator: true,
+      // now: () => {
+      //   return "2024-02-27T13:00:00";
+      // },
 
-      hiddenDays: [0, 1],
+      editable: true,
+
+      // hiddenDays: [0, 1],
       weekends: true,
       dateClick: (value) => {
+        //click space (not event exist)
         handleDateClick(value);
       },
       eventOverlap: false,
       headerToolbar: {
         left: "prev,next today",
-        center: "title",
+        right: "title",
+      },
+      footerToolbar: {
         right: "dayGridMonth,timeGridWeek,timeGridDay",
       },
+      eventMouseEnter: (info) => {
+        console.log("hover", info.event);
+      },
 
-      contentHeight: "auto",
+      contentHeight: 400,
       expandRows: true,
       buttonText: {
+        // timeGridWeek: "สัปดาห์",
         week: "สัปดาห์",
         day: "วัน",
         month: "เดือน",
         today: "วันนี้",
       },
+      eventChange: (changeInfo) => {
+        console.log("changeEvent", changeInfo.event);
+      },
+      eventClick: (clickInfo) => {
+        //click event exist
+        if (
+          confirm(
+            `Are you sure you want to delete the event '${clickInfo.event.title}'`
+          )
+        ) {
+          clickInfo.event.remove();
+        }
+      },
+      eventResize: function (info) {
+        alert(
+          info.event.title + " end is now " + info.event?.end?.toISOString()
+        );
+
+        if (!confirm("confirm your change")) {
+          info.revert();
+        }
+      },
+
       events: [
         {
           title: "โครงการพัฒนา พนักงานครั้งที่22",
-          start: "2024-02-27T10:00:00",
+          start: "2024-03-06T10:00:00",
+          interactive: true,
+          backgroundColor: "#ee5555",
+          borderColor: "#ee5555",
           // end: "2024-03-02",
-          overlap: false,
+          overlap: true,
           // display: "background",
         },
         {
           title: "asdf",
           start: "2024-02-27T16:00:00",
+          backgroundColor: "#ee5555",
+          borderColor: "#ee5555",
           // end: "2024-02-29",
           overlap: true,
           // display: "background",
@@ -163,7 +249,10 @@ export default {
     };
 
     const handleDateClick = (value: DateClickArg) => {
-      alert(`date click ${value.dateStr}`);
+      console.log(value);
+
+      // alert(`date click ${value.dateStr} ${JSON.stringify(value.view)}`);
+      // value.dayEl.style.backgroundColor = "red";
     };
 
     return {
@@ -184,4 +273,9 @@ export default {
   position: absolute;
   left: 11px;
 } */
+@media screen and (max-width: 767px) {
+  .fc-toolbar.fc-header-toolbar {
+    font-size: 60%;
+  }
+}
 </style>
